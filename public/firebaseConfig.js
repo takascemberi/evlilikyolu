f81444ac593f87ef8489f94339acaab0b1e11b4d
takascemberi <takascemberi@gmail.com>
2025-07-15 20:04:15 +0000

    firebaseConfig.js eklendi - Firebase bağlantısı yapıldı

---
+++ public/firebaseConfig.js
@@ -0,0 +1,17 @@
+// public/firebaseConfig.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcBbfYUNmzCUJ9woKsQeC4b7U4L3Td42w",
+  authDomain: "vipbulusma.firebaseapp.com",
+  projectId: "vipbulusma",
+  storageBucket: "vipbulusma.firebasestorage.app",
+  messagingSenderId: "552096712424",
+  appId: "1:552096712424:web:2c5afb59f00dcd24092b5d",
+  measurementId: "G-CNQEJEV51C"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
