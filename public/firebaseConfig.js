fb29542d82fc6db62625947b8b7089e09879a18b
takascemberi <takascemberi@gmail.com>
2025-07-19 14:45:15 +0300

    Update firebaseConfig.js

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -5,9 +5,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyAcBbfYUNmzCUJ9woKsQeC4b7U4L3Td42w",
   authDomain: "vipbulusma.firebaseapp.com",
   projectId: "vipbulusma",
-  storageBucket: "vipbulusma.firebasestorage.app",
+  storageBucket: "vipbulusma.appspot.com", // ← düzeltildi
   messagingSenderId: "552096712424",
   appId: "1:552096712424:web:2c5afb59f00dcd24092b5d",
+  measurementId: "G-CNQEJEV51C" // ← eksikti, geri eklendi
 };
 
 export const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
