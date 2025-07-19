8194aadd329baced23408f510406fcc0a2773c28
takascemberi <takascemberi@gmail.com>
2025-07-19 14:28:37 +0300

    Update firebaseConfig.js

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,16 +1,13 @@
 // public/firebaseConfig.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAcBbfYUNmzCUJ9woKsQeC4b7U4L3Td42w",
   authDomain: "vipbulusma.firebaseapp.com",
   projectId: "vipbulusma",
   storageBucket: "vipbulusma.firebasestorage.app",
   messagingSenderId: "552096712424",
   appId: "1:552096712424:web:2c5afb59f00dcd24092b5d",
-  measurementId: "G-CNQEJEV51C"
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
