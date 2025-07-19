ac2383e6d6130a24dfa867fabcd56e1b0227475e
takascemberi <takascemberi@gmail.com>
2025-07-19 16:23:06 +0300

    Update firebaseConfig.js

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,14 +1,18 @@
 // public/firebaseConfig.js
 import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAcBbfYUNmzCUJ9woKsQeC4b7U4L3Td42w",
   authDomain: "vipbulusma.firebaseapp.com",
   projectId: "vipbulusma",
-  storageBucket: "vipbulusma.appspot.com", // ← düzeltildi
+  storageBucket: "vipbulusma.appspot.com", // ← doğru
   messagingSenderId: "552096712424",
   appId: "1:552096712424:web:2c5afb59f00dcd24092b5d",
-  measurementId: "G-CNQEJEV51C" // ← eksikti, geri eklendi
+  measurementId: "G-CNQEJEV51C"
 };
 
-export const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const auth = getAuth(app);
+
+export { app, auth };
